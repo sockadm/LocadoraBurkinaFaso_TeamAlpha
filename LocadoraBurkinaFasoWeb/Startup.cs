8b3df7f21e702443a6d9bca12c559d2ea93c637c
sockadm <sock.adm@gmail.com>
2016-07-11 23:26:08 -0300

    1 - commit com MVC

---
+++ LocadoraBurkinaFasoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocadoraBurkinaFasoWeb.Startup))]
+namespace LocadoraBurkinaFasoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
